fe6de1a0158471b548019cf3852bc5e69646a22b
SevioCorrea <102820124+SevioCorrea@users.noreply.github.com>
2023-07-01 10:24:20 -0400

    First Commit

---
+++ AppleWatchApp Watch App/AppleWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleWatchAppApp.swift
+//  AppleWatchApp Watch App
+//
+//  Created by Sévio Basilio Corrêa on 01/07/23.
+//
+
+import SwiftUI
+
+@main
+struct AppleWatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

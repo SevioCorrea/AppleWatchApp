44f0851978ef4b2a6cd908bcf6880267a0a29726
SevioCorrea <102820124+SevioCorrea@users.noreply.github.com>
2023-07-06 09:34:53 -0400

    DetailView

---
+++ AppleWatchApp Watch App/View/DetailView.swift
@@ -0,0 +1,72 @@
+//
+//  DetailView.swift
+//  AppleWatchApp Watch App
+//
+//  Created by Sévio Basilio Corrêa on 06/07/23.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    // MARK: - Properties
+    let note: Note
+    let count: Int
+    let index: Int
+
+    // MARK: - Body
+    var body: some View {
+        VStack(alignment: .center, spacing: 3) {
+            // Header
+            HStack {
+                Capsule()
+                    .frame(height: 1)
+                
+                Image(systemName: "note.text")
+                
+                Capsule()
+                    .frame(height: 1)
+            } //: HStack
+            .foregroundColor(.accentColor)
+            
+            // Content
+            Spacer()
+            
+            ScrollView(.vertical) {
+                Text(note.text)
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.center)
+            }
+            
+            Spacer()
+            
+            // Footer
+            HStack(alignment: .center) {
+                Image(systemName: "gear")
+                    .imageScale(.large)
+                
+                Spacer()
+                
+                Text("\(count) / \(index + 1)")
+                
+                Spacer()
+                
+                Image(systemName: "info.circle")
+                    .imageScale(.large)
+                
+            } //: HStack
+            .foregroundColor(.secondary)
+            
+        } //: VStack
+        .padding(3)
+    }
+}
+
+// MARK: - Preview
+
+struct DetailView_Previews: PreviewProvider {
+    static var sampleData: Note = Note(id: UUID(), text: "Hello, World!")
+    static var previews: some View {
+        DetailView(note: sampleData, count: 5, index: 1)
+    }
+}

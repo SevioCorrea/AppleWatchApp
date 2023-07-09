a44f9d9d637a97927ae939718dab3543df9967a0
SevioCorrea <102820124+SevioCorrea@users.noreply.github.com>
2023-07-09 12:23:50 -0400

    Header

---
+++ AppleWatchApp Watch App/View/SettingsView.swift
@@ -0,0 +1,31 @@
+//
+//  SettingsView.swift
+//  AppleWatchApp Watch App
+//
+//  Created by Sévio Basilio Corrêa on 09/07/23.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    // MARK: - Properties
+
+    // MARK: - Body
+    var body: some View {
+        VStack(spacing: 8) {
+            // Header
+            HeaderView(title: "Settings")
+            
+            // Actual Line Count
+            // Slider
+        } //: vStack
+    }
+}
+
+// MARK: - Preview
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
